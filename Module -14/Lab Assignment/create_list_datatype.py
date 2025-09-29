0600814e6f3b00f1ccb8a492a7248aa173310951
RONAK TANK <154510469+ronaktank20@users.noreply.github.com>
2025-09-29 16:55:08 +0530

    .

--- Module -14/Lab Assignment/create_list_datatype.py
+++ Module -14/Lab Assignment/create_list_datatype.py
@@ -1,16 +1,23 @@
 #Write a Python program to create a list with elements of multiple data types (integers, 
 #strings, floats, etc.). 
 
-lst1 = [10,20,30,40]
-lst2 = [2.5,12.56,6.9,4.8]
-lst3 = ['monak','ronak','ustav']
+# Integer list
+lst1 = [10, 20, 30, 40]
 
+# Float list
+lst2 = [2.5, 12.56, 6.9, 4.8]
+
+# String list
+lst3 = ['monak', 'ronak', 'ustav']
+
+print("Integer List:")
 for i in lst1:
     print(i)
 
+print("\nFloat List:")
 for i in lst2:
     print(i)
 
+print("\nString List:")
 for i in lst3:
     print(i)
-
